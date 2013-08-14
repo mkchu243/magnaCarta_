b47c0900beb8eed9cd1e6ec3346837256ab6644a
mkchu243 <mkchu243@gmail.com>
2013-08-14 11:16:48 -0700

    causing conflicts

--- Assets/Scripts/Reference.cs
+++ Assets/Scripts/Reference.cs
@@ -16,7 +16,7 @@ public struct ElementAttributes{
 }
 
 static class Reference {
-  ////////////////TODO put your comment here/////////////////
+  ////////////////causing conflicts/////////////////
   public static HashSet<Element> WeaknessWater = new HashSet<Element> { Element.earth, Element.holy };
   public static HashSet<Element> WeaknessFire  = new HashSet<Element> { Element.water, Element.holy };
   //public static HashSet<Element> WeaknessWood  = new HashSet<Element> { Element.metal, Element.holy };
